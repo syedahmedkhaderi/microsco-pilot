2c4f1184dd85da1228f0abe95f9c666b7842536e
syedahmedkhaderi <syedahmedkhaderi@gmail.com>
2025-12-13 11:32:20 +0300

    Restarting the project

--- MicroscoPilot/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MicroscoPilot - Autonomous Microscopy Agent
-
-This package contains modules for autonomous microscopy exploration using
-Claude Vision API and DTMicroscope simulator.
-"""
-
-__version__ = "0.1.0"
-
